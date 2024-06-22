608cbb5fecfedcb5f9c50b76c3a72dce589826ac
Md Mobassher Hossain <mobassherpautex@gmail.com>
2024-06-22 13:04:04 +0600

    banner image added

---
+++ src/assets/index.ts
@@ -0,0 +1,26 @@
+const assets = {
+  icons: {
+    email: require("@/assets/icons/email.png"),
+    facebook: require("@/assets/icons/facebook.png"),
+    instagram: require("@/assets/icons/instagram.png"),
+    linkedIn: require("@/assets/icons/linkedin.png"),
+    pinterest: require("@/assets/icons/pinterest.png"),
+    reddit: require("@/assets/icons/reddit.png"),
+    twitter: require("@/assets/icons/twitter.png"),
+    whatsapp: require("@/assets/icons/whatsapp.png"),
+  },
+  banner: {
+    banner1: require("@/assets/banner/banner-1.png"),
+    banner2: require("@/assets/banner/banner-2.png"),
+    banner3: require("@/assets/banner/banner-3.png"),
+    banner4: require("@/assets/banner/banner-4.png"),
+    banner5: require("@/assets/banner/banner-5.png"),
+    banner6: require("@/assets/banner/banner-6.png"),
+    banner7: require("@/assets/banner/banner-7.png"),
+  },
+  admission: {
+    info: require("@/assets/adminssion/Admission Info.png"),
+  },
+};
+
+export default assets;
