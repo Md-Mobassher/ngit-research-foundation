850f2d4d382c1ca984685ddafbc619c70fcc502f
Md Mobassher Hossain <mobassherpautex@gmail.com>
2024-06-23 08:01:32 +0600

    favicon updated

--- src/app/publications/page.tsx
+++ src/app/publications/page.tsx
@@ -7,28 +7,12 @@ const PublicationsPage = () => {
   const years = Object.keys(publicationsByYear)
     .map(Number)
     .sort((a, b) => b - a);
-  console.log(years);
 
   return (
     <div className="mb-10 max-w-6xl mx-auto px-4">
       <PageTitle ctitle="Publication" />
 
       <div className="mt-8">
-        {/* {years.map((year) => (
-          <div key={year} className="mt-10">
-            <h2 className="text-2xl font-bold mb-4 text-center text-blue-500">
-              {year}
-            </h2>
-
-            {publicationsByYear[parseInt(Number(year))].length === 0 ? (
-              <p>No data found</p>
-            ) : (
-              publicationsByYear[parseInt(year)].map((pub, index) => (
-                <PublicationCard key={index} pub={pub} />
-              ))
-            )}
-          </div>
-        ))} */}
         {years.map((year) => (
           <div key={year} className="mt-10">
             <h2 className="text-2xl font-bold mb-4  text-blue-500">{year}</h2>
